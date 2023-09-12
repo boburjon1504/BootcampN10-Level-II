73a2c59b4c9ad9f093db69e2a1f0003f724e8ae0
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-09-12 20:59:26 +0500

    done N36 Ht1

---
+++ N36-HT-1/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Person
+- Employee
+- Manager
+ */
+
+public record Person(string FirstName, string LastName, string MiddleName, string Age);
+
+public record Employee(string FirstName, string LastName, string MiddleName, string Age, decimal Salary) : Person(FirstName, LastName, MiddleName, Age);
+public record Manager(string FirstName, string LastName, string MiddleName, string Age, decimal Salary) : Person(FirstName, LastName, MiddleName, Age);
--- N37-Task/Program.cs
+++ N37-Task/Program.cs
@@ -1,2 +1,11 @@
-﻿public abstract record Person(string FirstName,string LastName);
-public record Strudent(string FirstName,string LastName) : Person(FirstName,LastName);
+﻿//var student = new Strudent("bobur", "joraboyev");
+//student.Deconstruct(out string firstName, out string lastName);
+//Console.WriteLine(firstName);
+//public abstract record Person(string FirstName,string LastName);
+//public record Strudent(string FirstName,string LastName) : Person(FirstName,LastName);
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+
+
+
