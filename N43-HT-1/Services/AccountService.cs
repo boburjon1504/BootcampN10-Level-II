0311b134b8c2fc3b2e181dda287e265f3cdcbd72
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-09-28 20:00:40 +0500

    N45 home task done

--- N43-HT-1/Services/AccountService.cs
+++ N43-HT-1/Services/AccountService.cs
@@ -12,6 +12,7 @@ namespace N43_HT_1.Services
         public AccountService(UserService userService)=>_userService = userService;
         public async ValueTask<FileStream> CreateReportAsync(Guid id)
         {
+
             var employeeService = new EmployeeService(_userService);
             var performanceService = new PerformanceService(employeeService);
             return await performanceService.ReportPerformanceAsync(id);
---
+++ N45-HT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+var users = new List<User>()
+{
+    new (1,"Bob"),
+    new (2,"Bob"),
+    new (3,"Bob"),
+    new (4,"Bob")
+};
+var orders = new List<Order>
+{
+    new Order(1,10,2),
+    new Order(1,10,1),
+    new Order(1,10,1),
+};
+var products = new List<Product>
+{
+    new Product(1,"Coca Cola",12000),
+    new Product(1,"Coca Cola",12000),
+    new Product(1,"Coca Cola",12000),
+};
+var orderedProduct = new List<OrderProduct>
+{
+    new OrderProduct(1,1,2,10),
+    new OrderProduct(1,1,1,10),
+    new OrderProduct(1,1,1,10),
+};
+var query =
+    from user in users
+    join order in orders on user.Id equals order.UserId
+    select (user, order);
+
+public record User(int Id, string firstName);
+public record Order(int Id, decimal amount, int UserId);
+public record OrderProduct(int Id, int ProductId, int OrderId, int Count);
+public record Product(int Id, string Name, decimal Price);
