8212076216fded6bfdb7288dccc2ef80db6c37f9
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-10-26 01:34:00 +0500

    done N59 home task

---
+++ N59-HT-1/Controllers/DrivesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace N59_HT_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrivesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+           return Ok(DriveInfo.GetDrives().Select(dr => new StorageDrive
+            {
+                Name = dr.Name.Substring(0,dr.Name.IndexOf(":")),
+                Path = dr.Name,
+                Format = dr.DriveFormat,
+                TotalSize = dr.TotalSize,
+                TotalFreeSpace = dr.TotalFreeSpace,
+
+            }));
+
+        }
+    }
+}
---
+++ N59-HT-1/StorageDrive.cs
@@ -0,0 +1,12 @@
+﻿namespace N59_HT_1
+{
+    public class StorageDrive
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Format { get; set; }
+        public long TotalSize { get; set; }
+        public long TotalFreeSpace { get; set; }
+        public long AvaibleFreeSpace { get; set; }
+    }
+}
