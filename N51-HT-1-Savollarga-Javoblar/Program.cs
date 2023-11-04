9e0f2a6f4b854e55bd503d6684b2fd4a4cb98b4f
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-11-04 06:50:20 +0500

    done N51 home task

--- N51-HT-1-Savollarga-Javoblar/Program.cs
+++ N51-HT-1-Savollarga-Javoblar/Program.cs
@@ -5,3 +5,26 @@
   controller ichida nechta service inject qilish mumkin ?
   controller ichida foundations service larini ishlatish mumkinmi ?
  */
+
+
+/*
+ *  1 - javob
+ * barcha buyurtmalarni olish uchun bitta [HttpGet] va Get() , 
+ * bir buyurma bo'lgan tovarni olish uchun [HttpGet("{orderId:(Type)}"] bo'lishi kerak
+ */
+
+
+/*2- javob
+ * Configuration filelar asosan Api ni o'zida saqlanadi
+ */
+
+/*3- javob
+ * 
+ * controller ichiga cheksiz ko'p servicelarni registratsiya qilsa bo'ladi
+ */
+
+/*4- javob
+ * 
+ * Orchestretion servicelar orqali ishlatish maqsadga muvofiq bo'ladi
+ * 
+ */
