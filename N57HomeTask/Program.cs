59b00cb8d5a6175c35d1cd0f6779409c6c305fa5
boburjon1504 <boburjon67joraboyev@gmail.com>
2023-11-04 06:55:44 +0500

    done N57 home task

---
+++ N57HomeTask/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*savollar:
+ * 
+ * external configuration ga kirishi mumkun bo'lgan qiymatlarga misollar keltiring 
+ * 
+ * configuration modelga yozishimiz mumkun bo'lgan ma'lumot typlarini yozing
+ */
+
+
+/*1-javob:
+ * 
+ * external configurationga: appimiz qaysi environmentda run bo'layotgani haqida, 
+ * emailsetttingslar, database settings and etc.
+ */
+
+/*2- javob
+ * 
+ * configuration model ichida har qanday tipni saqlash mumkun
+ */
